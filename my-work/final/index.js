6bdb36f00665b391f1de92aec8b8c8102397b259
WenheLI <wl1508@nyu.edu>
2019-11-19 23:19:55 +0800

    add

--- my-work/final/index.js
+++ my-work/final/index.js
@@ -1,4 +1,4 @@
-let w = 1200;
+let w = 800;
 let h = 800;
 let padding = 90
 
@@ -11,8 +11,11 @@ const sum = (arr) => {
 const main = async () => {
     let viz = d3.select("#container").append("svg")
         .style("width", w)
-        .style("height", h);
+        .style("height", h)
+        .style('background', 'aliceblue');
+
     let data = await d3.csv('./videogames.csv');
+    const textArea = document.getElementById('text')
     data = data.filter((it) => it['Year_of_Release'] !== 'N/A')
         .filter((it) => it['Year_of_Release'] !== '2017')
         .filter((it) => it['Year_of_Release'] !== '2020')
@@ -63,29 +66,57 @@ const main = async () => {
         .append("path")
         .attr('class', 'line global')
         .attr("d", lineMaker)
-        .attr('stroke', 'aqua')
+        .attr('stroke', 'black')
         .attr('stroke-width', '5px')
         .style('fill', 'none');
 
     viz.selectAll('.point').data(salesData)
         .enter()
         .append('circle')
-        .attr('class','point')
+        .attr('class', 'point')
         .attr('cx', (d) => xScale(d.year))
         .attr('cy', (d) => yScale(d.sales))
         .attr('r', (d) => 5)
-        .style('fill', 'black')
+        .style('fill', '#c71585')
         .on('mouseenter', function (d) {
+            viz.append('line')
+                .attr('class', 'animatedLine')
+                .style("stroke", "#c71585")
+                .style("stroke-weight", 30)
+                .attr('x1', xScale(d.year))
+                .attr('y1', yScale(d.sales))
+                .attr('x2', xScale(d.year))
+                .attr('y2', yScale(d.sales))
+                .transition()
+                .duration(1000)
+                .attr('y2', yScale(0));
+            // viz.append('rect')
+            //     .attr('class', 'selectedArea')
+            //     .attr('x', xScale(d.year))
+            //     .attr('y', yScale(yMax))
+            //     .attr('width', 160)
+            //     .attr('height', yScale(0))
+            //     .style('opacity', .5)
+            //     .style('fill', 'black')
+            textArea.innerHTML = `<p>This is ${d.year.getFullYear()} </p> <br > <p> The global sales are ${d.sales.toFixed(2)} millions</p> <br >`
+
             d3.select(this)
-             .transition()
-             .style('fill', 'orange')
-             .attr('r', 10);
+                .transition()
+                .style('fill', 'orange')
+                .attr('r', 10);
         })
         .on('mouseleave', function (d) {
+            viz.selectAll('.selectedArea').remove();
+
+            viz.selectAll('.animatedLine')
+                .transition()
+                .duration(1000)
+                .attr('y2', yScale(d.sales))
+                .remove()
             d3.select(this)
-             .transition()
-             .style('fill', 'black')
-             .attr('r', 5);
+                .transition()
+                .style('fill', '#c71585')
+                .attr('r', 5);
         })
 
 }
